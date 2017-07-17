2ae0b1d5846060d69e83f12e6aaa368a86b78569
shadownater <jlovas@ualberta.ca>
2017-07-17 15:12:18 -0600

    base of project so far

---
+++ mysite/urls.py
@@ -0,0 +1,13 @@
+from django.conf.urls import include, url
+from django.contrib import admin
+from theactualsite import views
+
+urlpatterns = [
+    # Examples:
+    # url(r'^$', 'mysite.views.home', name='home'),
+    # url(r'^blog/', include('blog.urls')),
+
+    url(r'^$', views.index, name='index'),
+    url(r'^admin/', include(admin.site.urls)),
+    #url(r'^theactualsite/', include('theactualsite.urls', namespace='theactualsite')),
+]
