dfa850059f7895848411ddad12e14b0a3ab1c9db
shadownater <jlovas@ualberta.ca>
2017-07-24 21:07:54 -0600

    got my new lines working

--- theactualsite/views.py
+++ theactualsite/views.py
@@ -11,7 +11,14 @@ def artPage(request):
 
     #fetch art appropriately
     #probably just go from year and back, not really sure how/if I should categorize this like I have with code stuff
-    art_list = ArtImg.objects.all()
+    t_art_list = ArtImg.objects.all()
+
+    art_list = []
+    for item in t_art_list:
+        if not(itemExists(item, art_list) ):
+            #gets here when item is not in art_list
+            art_list.append(item)
+
 
     context = {
         'art_list': art_list,
@@ -24,3 +31,12 @@ def artPage(request):
 #Programming page
 def programmingPage(request):
     return HttpResponse("This is where the programming stuff will be!!!")
+
+
+def itemExists(a, list):
+    result = False
+    for i in list:
+        if a.associated_piece == i.associated_piece:
+            result = True
+            return result
+    return False
