d0df2e201e2ee704f125d56f25f0b8d855d8cd85
shadownater <jlovas@ualberta.ca>
2017-07-27 11:20:24 -0600

    images given better alignment to their clipping boxes

--- theactualsite/static/portfolioJS.js
+++ theactualsite/static/portfolioJS.js
@@ -2,7 +2,7 @@
  * Created by Jill - Offline on 2017-07-25.
  */
 
-
+//for the cool neato way the title shows on hover
 $( ".handmadeCrop" ).hover(
   function() {
 
@@ -18,3 +18,33 @@ $( ".handmadeCrop" ).hover(
 
   }
 );
+
+
+
+//for centering the image nicely
+//Y axis is not 100% centered- tried out that art thing where you leave more space below
+$(".cardImg").each(function(){
+
+    var img_width = $(this).width();
+    var img_height = $(this).height();
+    var clippingBoxW = $(".handmadeCrop").width();
+    var clippingBoxH = $(".handmadeCrop").height();
+
+    //centers of both the image itself and the clipping box
+    var centeringW = img_width/2.0;
+    var centeringH = img_height/2.0;
+    var clippingCenterW = clippingBoxW/2.0;
+    var clippingCenterH = clippingBoxH/2.0;
+
+    centeringW -= clippingCenterW;
+    centeringH = centeringH - clippingCenterH + (clippingCenterH/5);
+
+    centeringW *=(-1);
+    centeringH *=(-1);
+
+    $(this).css('margin-left', centeringW);
+    $(this).css('margin-top', centeringH);
+
+});
+
+//css({'margin-top':'300px'});
