d2c55c7ace7f789132154d0380aac50d11ce85f9
shadownater <jlovas@ualberta.ca>
2017-08-19 19:15:25 -0600

    fixed bouncy carousel issue

---
+++ theactualsite/static/detailJS.js
@@ -0,0 +1,20 @@
+/**
+ * Created by Jill - Offline on 2017-08-19.
+ *
+ */
+
+
+//this function will make the carousel the appropriate min-height for the image
+
+var myImages = $(".imageResizing");
+
+var maxHeight = 0;
+
+//store the height of the largest image, then use that to set the min height of the carousel
+for(var i = 0; i < myImages.length; i++){
+    if(myImages[i].height > maxHeight){
+        maxHeight = myImages[i].height;
+    }
+}
+
+$("#picCarousel").css('min-height', maxHeight);
