c3a3a1046428b6cf7068f47b4fbf2a0a78bd14b7
shadownater <jlovas@ualberta.ca>
2017-08-22 15:05:25 -0600

    admin stuffs

--- theactualsite/admin.py
+++ theactualsite/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Art, Programming, ArtImg, ProgImg
 
 # Register your models here.
+admin.site.register(Art)
+admin.site.register(ArtImg)
+admin.site.register(Programming)
+admin.site.register(ProgImg)
