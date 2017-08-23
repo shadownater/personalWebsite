22f187f71df9605e0671c169cb3f00c31087cfdb
shadownater <jlovas@ualberta.ca>
2017-08-23 17:06:33 -0600

    changed static thing to way that sorta worked neither really do

--- mysite/settings.py
+++ mysite/settings.py
@@ -108,7 +108,7 @@ STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = (
-    os.path.join(PROJECT_DIR, 'static'),
+    #os.path.join(PROJECT_DIR, 'static'),
     os.path.join(PROJECT_ROOT, '../theactualsite/static'),
 )
 
